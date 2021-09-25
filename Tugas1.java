1cd243ef7d00964ba51068d36626c2d388fa3fb8
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-09-26 05:43:45 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,10 @@
+
+public class Tugas1 {
+
+    public static void main(String[] args) {
+       System.out.println("Belajar JAVA");
+       System.out.println("Sangat mudah sekali");
+       System.out.println("dan sangat menyenangkan");
+    }
+    
+}
