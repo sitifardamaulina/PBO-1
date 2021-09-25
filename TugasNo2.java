634b41ff807913fe826ffc16596f681af1faeb99
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-09-26 05:44:21 +0700

    Add files via upload

---
+++ TugasNo2.java
@@ -0,0 +1,12 @@
+package java1.pkg1;
+import javax.swing.JOptionPane;
+
+public class TugasNo2 {
+
+    
+    public static void main(String[] args) {
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar Pemrogaraman Web "+namaTeman+" Sangat mudah ");
+    }
+    
+}
